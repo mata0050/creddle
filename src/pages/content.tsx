b11d2244b46dbc9353c0cf57688ae47a633563be
mata0050 <mata0050@outlook.com>
2022-07-16 23:30:14 -0400

    planetscale db connected, zustand added

---
+++ src/pages/content.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Content = () => {
+  return (
+    <div className='pt-48'>Content</div>
+  )
+}
+
+export default Content
---
+++ src/store.ts
@@ -0,0 +1,21 @@
+import create from 'zustand';
+
+type Store = {
+  bears: number;
+  education: [];
+  employment: [];
+  project: [];
+  skill: [];
+  user: [];
+
+};
+const useStore = create<Store>((set) => ({
+  bears: 0,
+  education: [],
+  employment: [],
+  project: [],
+  skill: [],
+  user: [],
+  increasePopulation: () => set((state) => ({ bears: state.bears + 1 })),
+  removeAllBears: () => set({ bears: 0 }),
+}));
