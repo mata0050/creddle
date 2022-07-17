2fe8db7505a5933e59db1ed531bbe6def672e8b4
mata0050 <mata0050@outlook.com>
2022-07-16 23:58:34 -0400

    connecting to db test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,16 +2,28 @@ import type { NextPage } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
 import { trpc } from '../utils/trpc';
+import { prisma } from '../db/prisma';
 
-const Home: NextPage = () => {
+const Home: NextPage = (props : any) => {
   const utils = trpc.useContext();
   const usersQuery = trpc.useQuery(['user.getAllUsers']);
-  console.log(usersQuery.data);
+  console.log(props.users);
 
   return (
-    <div>
+    <div className='pt-48 px-[400px]'>
+      <h1>hello</h1>
+      <code>{props.users}</code>
     </div>
   );
+};
+
+export const getServerSideProps = async () => {
+  const users = await prisma.user.findMany();
+  return {
+    props: {
+      users : JSON.stringify(users),
+    },
+  };
 };
 
 export default Home;
